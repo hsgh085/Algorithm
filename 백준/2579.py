970c5333f759a8ca517542f273f4257a1dcdfaf9
hsgh085 <a01089835465@gmail.com>
2022-11-28 04:50:55 +0900

    백준 계단오르기

---
+++ 백준/2579.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open("input.txt", "rt")
+
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+
+dp = [0]*10000
+dp[0] = arr[0]
+
+if n>1:
+    dp[1] = arr[0]+arr[1]
+
+if n>2:
+    dp[2] = max(arr[0]+arr[2], arr[1]+arr[2])
+
+for i in range(3, n):
+    dp[i] = max(dp[i-3]+arr[i-1], dp[i-2])+arr[i]
+
+print(dp[n-1])
