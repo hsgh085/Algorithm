92fbc00d14287fc15384de2320384df79e6baf98
hsgh085 <a01089835465@gmail.com>
2023-01-22 23:33:30 +0900

    20230122

--- 백준/문자열/단어정렬.py
+++ 백준/문자열/단어정렬.py
@@ -1,2 +1,14 @@
 import sys
 sys.stdin = open("input.txt", "rt")
+input=sys.stdin.readline
+n=int(input())
+arr=[[] for _ in range(51)]
+for _ in range(n):
+    s=input().rstrip()
+    arr[len(s)].append(s)
+for i in range(51):
+    if arr[i]:
+        setArr=list(set(arr[i]))
+        setArr.sort()
+        for x in setArr:
+            print(x)
